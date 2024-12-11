6da958f7163d96e3b144f27c4bd7a59e4ff2a38a
ZhipengZhang-flexiv <18595628284@163.com>
2024-12-11 11:45:04 +0800

    fix cmake option bug

--- thirdparty/scripts/install_eigen.sh
+++ thirdparty/scripts/install_eigen.sh
@@ -24,7 +24,7 @@ cmake .. -DCMAKE_BUILD_TYPE=Release \
          -DBUILD_SHARED_LIBS=OFF \
          -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
          -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-         -DCAMEK_PREFIX_PATH=$INSTALL_DIR 
+         -DCMAKE_PREFIX_PATH=$INSTALL_DIR 
 
 # Build and install
 cmake --build . --target install --config Release -j $NUM_JOBS
