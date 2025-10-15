49b9d12bdf952801e1a63d41fe3f7dcfe64bb584
Peizhang Zhu <peizhang.zhu@flexiv.com>
2025-10-15 11:14:06 -0700

    Format files

--- example/basics3_display_plan_info.cpp
+++ example/basics3_display_plan_info.cpp
@@ -21,78 +21,82 @@ std::atomic<bool> keep_running(true);
 /**
  * @brief Handler for user stop interaction
  */
-void SignalHandler(int signal) {
-  if (signal == SIGINT) {
-    keep_running = false;
-    spdlog::info("Signal received, stopping...");
-  }
+void SignalHandler(int signal)
+{
+    if (signal == SIGINT) {
+        keep_running = false;
+        spdlog::info("Signal received, stopping...");
+    }
 }
 
 /** @brief Print program usage help */
-void PrintHelp() {
-  // clang-format off
+void PrintHelp()
+{
+    // clang-format off
     std::cout << "Required arguments: [robot SN]" << std::endl;
     std::cout << "    robot SN: Serial number of the robot to connect to. "
                  "Remove any space, for example: Rizon4s-123456" << std::endl;
     std::cout << "Optional arguments: None" << std::endl;
     std::cout << std::endl;
-  // clang-format on
+    // clang-format on
 }
 
 /** @brief Print plan info @ 1Hz */
-void printPlanInfo(flexiv::ddk::Client &client) {
-  while (keep_running.load()) {
-    // Check connection with the robot
-    if (!client.connected()) {
-      spdlog::error("Cannot connect with robot, retrying...");
-      std::this_thread::sleep_for(std::chrono::seconds(5));
-      continue;
+void printPlanInfo(flexiv::ddk::Client& client)
+{
+    while (keep_running.load()) {
+        // Check connection with the robot
+        if (!client.connected()) {
+            spdlog::error("Cannot connect with robot, retrying...");
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+            continue;
+        }
+        // Print all robot states in JSON format using the built-in ostream operator
+        // overloading
+        spdlog::info("Current robot plan info:");
+        std::cout << client.plan_info() << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
-    // Print all robot states in JSON format using the built-in ostream operator
-    // overloading
-    spdlog::info("Current robot plan info:");
-    std::cout << client.plan_info() << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-  }
 }
 
-int main(int argc, char *argv[]) {
-  // Program Setup
-  // =============================================================================================
-  // Parse parameters
-  if (argc < 2 ||
-      flexiv::ddk::utility::ProgramArgsExistAny(argc, argv, {"-h", "--help"})) {
-    PrintHelp();
-    return 1;
-  }
-  // Serial number of the robot to connect to. Remove any space, for example:
-  // Rizon4s-123456
-  std::string robot_sn = argv[1];
+int main(int argc, char* argv[])
+{
+    // Program Setup
+    // =============================================================================================
+    // Parse parameters
+    if (argc < 2 || flexiv::ddk::utility::ProgramArgsExistAny(argc, argv, {"-h", "--help"})) {
+        PrintHelp();
+        return 1;
+    }
+    // Serial number of the robot to connect to. Remove any space, for example:
+    // Rizon4s-123456
+    std::string robot_sn = argv[1];
 
-  // Print description
-  spdlog::info(">>> Tutorial description <<<\nThis tutorial check connection "
-               "with the robot and print plan info.");
+    // Print description
+    spdlog::info(
+        ">>> Tutorial description <<<\nThis tutorial check connection "
+        "with the robot and print plan info.");
 
-  // Setup signal handler for graceful exit
-  std::signal(SIGINT, SignalHandler);
-  try {
-    // DDK Initialization
-    // =========================================================================================
-    // Instantiate DDK client interface
-    flexiv::ddk::Client client(robot_sn);
+    // Setup signal handler for graceful exit
+    std::signal(SIGINT, SignalHandler);
+    try {
+        // DDK Initialization
+        // =========================================================================================
+        // Instantiate DDK client interface
+        flexiv::ddk::Client client(robot_sn);
 
-    // Print States
-    // =========================================================================================
-    // Use std::thread to do scheduling so that this example can run on all OS
-    std::thread low_priority_thread(std::bind(printPlanInfo, std::ref(client)));
+        // Print States
+        // =========================================================================================
+        // Use std::thread to do scheduling so that this example can run on all OS
+        std::thread low_priority_thread(std::bind(printPlanInfo, std::ref(client)));
 
-    // Properly exit thread
-    low_priority_thread.join();
+        // Properly exit thread
+        low_priority_thread.join();
 
-  } catch (const std::exception &e) {
-    spdlog::error(e.what());
-    return 1;
-  }
+    } catch (const std::exception& e) {
+        spdlog::error(e.what());
+        return 1;
+    }
 
-  return 0;
+    return 0;
 }
